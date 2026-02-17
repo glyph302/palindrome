7f908dddfaa6733db49c2ab9ab719570dd89cafd
boop <ag2841@srmist.edu.in>
2026-02-17 10:22:04 +0530

    UC1

--- project/src/Main.java
+++ project/src/Main.java
@@ -3,11 +3,6 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+    System.out.println("hello to the palindrome checker management system\nVersion : 1.0\nSystem initialized successfully.");
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
 }
