369979fefce34aa0cafa3dcba64894fc57f1391b
Vishruth <vt6089@srmist.edu.in>
2026-02-17 10:10:48 +0530

    Added UC3 + fixed UC1 & UC2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
-public class Main {
+ public class Main {
 
     public static void main(String[] args) {
 
         // Create objects
         PalindromeWelcome welcome = new PalindromeWelcome();
         PalindromeHardcode ph = new PalindromeHardcode();
+        PalindromeReverse rv = new PalindromeReverse();
 
         // Call methods
         welcome.displayMessage();
         System.out.println("checking for madam: " + ph.hardcodePaliCheck("madam"));
         System.out.println("checking for hello " + ph.hardcodePaliCheck("hello"));
+        System.out.println("Hello reversed is: " + rv.reverseString("hello"));
     }
 }
